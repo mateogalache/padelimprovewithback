bb35093af4c5215a6c2e0ebb17ef55fa428fc1b7
mateo.galache <mateo.galache12@gmail.com>
2025-03-04 23:05:03 +0100

    react router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,40 @@
 import { Header } from './Header.jsx'
+import { useState } from 'react';
 
-export function App() {
-    return (
-          <Header />
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+    window.history.pushState({}, '',href)
+    const navigationEvent = new Event(NAVIGATION_EVENT)
+    window.dispatchEvent(navigationEvent)
+}
+
+function SignUp() {
+    return(
+        <>  
+            <h1>Esto es el signup</h1>
+        </>
     )
 }
+
+export function App() {
+    const[currentPath, setCurrentPath] = useState(window.location.pathname)
+    
+    useEffect(() => {
+        const onLocationChange = () => {
+            setCurrentPath(window.location.pathname)
+        }
+        window.addEventListener(NAVIGATION_EVENT,onLocationChange)
+        return () =>{
+            window.removeEventListener(NAVIGATION_EVENT,onLocationChange)
+        }
+    }, [])
+
+    return (
+        <main>
+            {currentPath == '/' && <Header/>}
+            {currentPath == '/signup' && <SignUp/>}
+        </main>
+    )
+}
+
--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -4,8 +4,8 @@ export function Header() {
             <button>Explore</button>
             <h1 className="primary-color font-bold text-3xl">PADEL IMPROVE</h1>
             <div className="flex items-center gap-3">
-                <button className="border-2 rounded-3xl p-[8px] primary-color">Sign Up</button>
-                <button className="border-2 rounded-3xl p-[8px] bg-primary-color border-primary-color text-black">Log in</button>
+                <a href="/signup"><button className="border-2 rounded-3xl p-[8px] primary-color">Sign Up</button></a>
+                <a href=""><button className="border-2 rounded-3xl p-[8px] bg-primary-color border-primary-color text-black">Log in</button></a>
             </div>
         </header>
     )
